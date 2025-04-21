a2ade3294c06e2843c30c4abd47afb01c0e56418
GabrielFig <jgabriell_00@hotmail.com>
2025-04-20 23:28:46 -0700

    Refactor CustomerBase to use SQLModel and add field constraints

--- models.py
+++ models.py
@@ -1,25 +1,24 @@
-from pydantic import BaseModel
-from pydantic import EmailStr
-from datetime import datetime
+from pydantic import BaseModel, EmailStr
+from sqlmodel import SQLModel, Field
 
 # class BaseModel(Base):
 #     id: int
 #     #create_date: datetime
 #     #update_date: datetime | None
 
-class CustomerBase(BaseModel):
-    name: str
-    description: str | None
-    email: EmailStr
-    age: int
+class CustomerBase(SQLModel):
+    name: str = Field(max_length=50)
+    description: str | None = Field(default=None)
+    email: EmailStr = Field(max_length=50)
+    age: int = Field(default=0)
 
 
 class CustomerCreate(CustomerBase):
     pass
 
 
-class Customer(CustomerBase):
-    id: int | None = None
+class Customer(CustomerBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
 
 class Transaction(BaseModel):
     amount: int
